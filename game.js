1c590ec78a6d6571c24ad429b653fef162be2cf1
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-02 15:58:33 -0700

    Add event listener to buttons

--- game.js
+++ game.js
@@ -64,3 +64,11 @@ function game() {
         return "Sorry, you lost the game."
     }
 }
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    })
+})
